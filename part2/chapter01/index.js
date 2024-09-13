a7c399b0b1695e6015070781dccb7b13a6face3f
jdlee <jaedurk@naver.com>
2024-09-14 01:35:22 +0900

    part2 진행중

---
+++ part2/chapter01/index.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector('canvas');
+
+const ctx = canvas.getContext('2d');
+const dpr = window.devicePixelRatio;
+
+const canvasWidth = 300;
+const canvasHeight = 300;
+
+
+canvas.style.width = canvasWidth + 'px';
+canvas.style.height = canvasHeight + 'px';
+
+canvas.width = canvasWidth * dpr;
+canvas.height = canvasHeight * dpr;
+ctx.scale(dpr, dpr);
+
+// ctx.fillRect(10, 10, 50, 50);
+
+class Particle {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+const x = 100;
+const y = 100;
+const radius = 50;
+const particle = new Particle(x, y, radius);
+
+particle.draw();
+
+function animate() {
+    window.requestAnimationFrame(animate);
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    particle.draw();
+}
+
+animate();
